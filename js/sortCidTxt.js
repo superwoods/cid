8c2d6b0e10ae2133529b776f69fa7e57bdc2c879
SuperMoo <st_sister@me.com>
2020-07-28 15:57:45 +0800

    Update 0.1.1

--- js/sortCidTxt.js
+++ js/sortCidTxt.js
@@ -1,39 +1,25 @@
 const sortCidTxt = (txt) => {
     let resultArray = '';
+
     txt.split('\n').map((e, i) => {
         let _array = e.split(/\｜|\|/);
 
         console.log(_array);
 
-        resultArray += `    {
-        name: '${(_array[0]).replace(/^\s+|\s+$/g, "")}',
-        cid: ${_array[1] - 0},
-        ${_array[2] && _array[2] - 0 ? `nid: ${_array[2] - 0}`, : ''}
-        nav: true,
-        isHomepageChn: true,
-        isBlank: false,
-        mod: "--"
-    },`;
-        /**
-         * {
-                name: _array[0],
-                nav: true, // 是否在导航显示
-                isHomepageChn: true, // 是否是首页频道（必须要在导航显示才能生效）
-                // url: `/ page / ${ __cid } `,
-                // classNameKey: "classNameKey", // 类名识别关键字必须等同于 url 后半段
-                cid: __cid,
-                // nid: _array[2] - 0,
-                isBlank: false,
-                mod: "--"
-            },`;
-         */
-
-        // }
+        resultArray += `{
+    name: '${(_array[0]).replace(/^\s+|\s+$/g, "")}',
+    cid: ${_array[1] - 0},${_array[2] && _array[2] - 0 > 0 ? `\n    nid: ${_array[2] - 0},` : ''}
+    nav: true,
+    isHomepageChn: true,
+    isBlank: false,
+    mod: "--"
+},`;
     });
 
     console.log(resultArray);
+
     $('.json').html(`const cid = [
-${resultArray}
+    ${resultArray}
 ];`);
 };
 
